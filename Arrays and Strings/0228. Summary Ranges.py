7e66c590e305a665b8e688258eee03350b4744a4
mondalr3 <rodoshi.mondal@mail.utoronto.ca>
2025-06-09 23:01:04 -0400

    Added Summary ranges

---
+++ Arrays and Strings/0228. Summary Ranges.py
@@ -0,0 +1,32 @@
+def summaryRanges(nums:list[int]) -> list[int]:
+    """
+
+    You are given a sorted unique integer array nums.
+
+    A range [a,b] is the set of all integers from a to b (inclusive).
+
+    Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+    Each range [a,b] in the list should be output as:
+
+    "a->b" if a != b
+    "a" if a == b
+
+    """
+    ans = []
+    i = 0
+
+    while i < len(nums):
+        start = nums[i]
+        while i < len(nums)-1 and nums[i] + 1 == nums[i + 1]:
+            i += 1
+
+        if start != nums[i]:
+            ans.append(str(start) + "->" + str(nums[i]))
+        else:
+            ans.append(str(nums[i]))
+
+        i += 1
+
+
+
