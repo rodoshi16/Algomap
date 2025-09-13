f64ddc823ca63ead703f693776971e5ef4937876
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2025-09-13 15:05:42 -0400

    added

---
+++ graphs/graph.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+from typing import Any 
+
+class _Vertex:
+    """
+        Instance attributes:
+
+        -item: the data stored in the vertex
+        -neighbours: the set of vertices adjacent to this vertex
+    
+        Representation invariants:
+
+        - self cannot be in self.neighbours
+        -all(self in u.neighbours for u in self.neighbours)
+    
+    """
+
+    # can be int, string, any type
+    item: Any 
+    neighbours: set[_Vertex]
+
+
+    def __init__(self, item: Any, neighbours: set[_Vertex]) -> None:
+        self.item = item
+        self.neighbours = neighbours 
+    
+
+class Graph:
+
+    def __init__(self) -> None:
+        #dict
+        self._vertices = {}
+
+    def add_vertex(self, item: Any) -> None:
+
+        #add key value pair where item is the key and the value is the vertex
+        self._vertices[item] = _Vertex(item, set())
+
+    def add_edge(self, item1: Any, item2: Any) -> None:
+
+        # 1. Must check if the vertices actually exist, do a key lookup
+        # 2. Define the vertices
+        # 3. Add the opposite vertices as neighbours to each vertex 
+
+
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item]
+            v2 = self._vertices[item]
+        
+            v1.neighbours.add(v2)
+            v2.neighbours.add(v1)
+        
+        else:
+            raise ValueError 
+    
+    def adjacent(self, item1: Any, item2: Any) -> bool:
+
+        #
+    
+
+    
+
