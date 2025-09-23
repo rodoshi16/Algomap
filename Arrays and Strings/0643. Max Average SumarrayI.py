f6e83b920928ec50a3b1f7259e7576b744cf2288
rodoshi16 <rodoshi524@gmail.com>
2025-09-23 11:27:38 -0400

    added max average subarray

--- Arrays and Strings/0643. Max Average SumarrayI.py
+++ Arrays and Strings/0643. Max Average SumarrayI.py
@@ -1,9 +1,13 @@
-def findMaxAverage(self, num, k: int) -> float:
-    """"
+def findMaxAverage(num, k: int) -> float:
+    """
     You are given an integer array nums consisting of n elements, and an integer k.
 
     Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
     Example 1:
+
+    >>> findMaxAverage([1,12,-5,-6,50,3], 4)
+    12.75000
+
 
     Input: nums = [1,12,-5,-6,50,3], k = 4
     Output: 12.75000
@@ -20,8 +24,25 @@ def findMaxAverage(self, num, k: int) -> float:
     1 <= k <= n <= 105
     -104 <= nums[i] <= 104
 
-
-
-
-
     """
+
+    curr_sum = 0
+    max_avg = 0
+    for i in range(k):
+        curr_sum += num[i]
+
+    max_avg = curr_sum / k
+
+    for i in range(k, len(num)):
+        curr_sum += num[i]
+        curr_sum -= num[i - k]
+
+        avg = curr_sum / k
+        max_avg = max(avg, max_avg)
+
+    return max_avg
+
+
+
+
+
