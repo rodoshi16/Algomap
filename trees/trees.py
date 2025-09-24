fa52e8a6b9a00fbe609778a8d01b687299e2b646
Rodoshi Mondal <165502326+rodoshi16@users.noreply.github.com>
2025-09-23 23:25:25 -0400

    tree length method

--- trees/trees.py
+++ trees/trees.py
@@ -20,3 +20,28 @@ class Tree:
     
     def is_empty(self) -> bool:
         return self._root is None 
+    
+    def __len__(self) -> int:
+        """
+
+        The length is the same as the size of a tree. 
+
+        Base case: just root
+        recursive: we sum the size of each subtree + add one for the root 
+
+        >>> t = Tree(7, [Tree(1, [Tree(2), Tree(3)]), Tree(9, [Tree(4), Tree(5)])])
+        >>> t.__len__()
+        7 
+        
+        
+        
+        """
+        length = 0 
+        if self.is_empty:
+            return 0 
+        elif self._subtrees == []:
+            return 1 
+        else:
+            for subtree in self._subtrees:
+                length += subtree.__len__()
+        return length + 1
