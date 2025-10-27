0d346e9f23cc3fd5b045a4f6fb1d7f2026b95b41
rodoshi16 <rodoshi524@gmail.com>
2025-10-27 11:56:01 -0400

    Leetcode 0347

---
+++ Arrays and Strings/0347 Top K Frequent Elements.py
@@ -0,0 +1,62 @@
+def topKFrequent(nums: list[int], k: int) -> list[int]:
+    """
+
+    Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+    Example 1:
+
+    Input: nums = [1,1,1,2,2,3], k = 2
+
+    Output: [1,2]
+
+    Example 2:
+
+    Input: nums = [1], k = 1
+
+    Output: [1]
+
+    Example 3:
+
+    Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+
+    Output: [1,2]
+
+
+
+    Constraints:
+
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+    k is in the range [1, the number of unique elements in the array].
+    It is guaranteed that the answer is unique
+
+    >>> topKFrequent([1], 1)
+    [1]
+    >>> topKFrequent([1,2,3], 3)
+    [1, 2, 3]
+    >>> topKFrequent([1,1,1,2,2,3], k = 2)
+    [1, 2]
+    >>> topKFrequent([1,2,1,2,1,2,3,1,3,2], k = 2)
+    [2, 1]
+    >>> topKFrequent([3,3,3,3,3,3,2,2,2,2,2,2,1,1,1,1,1,1], 3)
+    [1, 2, 3]
+    >>> topKFrequent([3,3,3,3,3,3,2,2,2,2,2,2,1,1,1,1,1,1], 1)
+    [1]
+
+
+
+    """
+    if len(nums) == k:
+        return nums
+
+    d = {}
+
+    for i in range(len(nums)):
+        if nums[i] not in d:
+            d[nums[i]] = 1
+        else:
+            d[nums[i]] += 1
+
+    sorted_dict = sorted(d, key=lambda k: d[k])
+    return sorted_dict[::-1][:k]
+
+
