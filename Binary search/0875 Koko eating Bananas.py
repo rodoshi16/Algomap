6f5936ec16d2e513cfc074210303698bbcb70a08
rodoshi16 <rodoshi524@gmail.com>
2025-11-04 23:59:13 -0500

    test cases added

--- Binary search/0875 Koko eating Bananas.py
+++ Binary search/0875 Koko eating Bananas.py
@@ -18,6 +18,47 @@ def minEatingSpeed(piles: list[int], h: int) -> int:
     >>> minEatingSpeed([3,6,7,11], 8)
     4
 
+    >>> minEatingSpeed([30,11,23,4,20], 5)
+    30
+
+    >>> minEatingSpeed([30,11,23,4,20], 6)
+    23
+
+    >>> minEatingSpeed([312884470], 312884469)
+    2
+
+    >>> minEatingSpeed([1], 1)
+    1
+
+    >>> minEatingSpeed([1, 1, 1, 1], 4)
+    1
+
+    >>> minEatingSpeed([9, 9, 9], 3)
+    9
+
+    >>> minEatingSpeed([9, 9, 9], 6)
+    5
+
+    >>> minEatingSpeed([1000000000, 1000000000], 3)
+    666666667
+
+    >>> minEatingSpeed([805306368,805306368,805306368], 1000000000)
+    1
+
+    >>> minEatingSpeed([1,2,3,4,5,6,7,8,9], 100)
+    1
+
+
+    >>> minEatingSpeed([10**9], 1)
+    1000000000
+
+    >>> minEatingSpeed([1, 10**9], 2)
+    1000000000
+
+    >>> minEatingSpeed([1, 10**9], 3)
+    500000000
+
+
     """
     l = 1
     r = max(piles)
