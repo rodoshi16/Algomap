8fd3ba70164f4de0f168cbc4a581102836f07cc9
rodoshi16 <rodoshi524@gmail.com>
2025-11-05 23:18:04 -0500

    test cases for fisher yates

--- Algorithms/fisher_yates.py
+++ Algorithms/fisher_yates.py
@@ -6,6 +6,20 @@ def fisher_yates(l: list):
 
     Implement fisher yates algorithm on the list l
 
+    >>> random.seed(0)
+    >>> lst = [1, 2, 3, 4, 5]
+    >>> fisher_yates(lst)
+    [4, 5, 1, 3, 2]
+
+    >>> random.seed(123)
+    >>> fisher_yates([])
+    []
+
+    >>> random.seed(99)
+    >>> fisher_yates([1])
+    [1]
+
+
     """
 
     for i in range(len(l)-1, 0, -1):
