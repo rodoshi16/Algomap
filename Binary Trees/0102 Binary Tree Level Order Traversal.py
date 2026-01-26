886c9b63856b4584ba23778833b563e00e3d1315
rodoshi16 <rodoshi524@gmail.com>
2026-01-26 15:55:17 -0500

    time and space

--- Binary Trees/0102 Binary Tree Level Order Traversal.py
+++ Binary Trees/0102 Binary Tree Level Order Traversal.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+from collections import deque
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -31,11 +32,20 @@ def levelorder(root):
     The number of nodes in the tree is in the range [0, 2000].
     -1000 <= Node.val <= 1000
 
+    Time complexity: 0(n), n = number of nodes in the tree
+    Space:
+
+    - 0(m): max number of nodes in one level in a queue
+    - 0(n): lst will grow to the size of at most n nodes
+
+    Overall, Space: 0(n)
+
+
     """
     if root is None:
         return []
 
-    q = [root]
+    q = deque()
     lst = []
 
     #
@@ -45,7 +55,7 @@ def levelorder(root):
         visited = []
 
         for i in range(level_size):
-            node = q.pop(0)
+            node = q.popleft()
             visited.append(node.val)
 
             if node.left:
