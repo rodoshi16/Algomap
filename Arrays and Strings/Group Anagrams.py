3c6b54fe955f8ec385e16c63bf2f480feb86e15a
rodoshi16 <rodoshi524@gmail.com>
2026-01-30 20:09:40 -0500

    optimal group anagrams

--- Arrays and Strings/Group Anagrams.py
+++ Arrays and Strings/Group Anagrams.py
@@ -1,10 +1,40 @@
+from collections import defaultdict
 def groupAnagrams(strs: list[str]):
     """
     >>> groupAnagrams(["act", "pots", "tops", "cat", "stop", "hat"])
     [["act", "cat"], ["pots", "tops", "stop"], ["hat"]]
 
+
+    Time:
+
+    - for loop runs for every char in all strings
+    - n: len(strs), k: max length of longest word
+    - 0(n*k)
+
+    Space:
+
+    - sig: 0(26) = 0(1)
+    - d: worst case all sig are diff len(strs) keys 0(n)
+    - values: 0(n*k)
+
+    Total: 0(n) + 0(n*k) ~ 0(n*K)
+
     """
 
+    #optimal
+
+    d = defaultdict(list)
+    for s in strs:
+        sig = [0] * 26
+
+        for c in s:
+            sig[ord(c) - ord("a")] += 1
+
+        d[tuple(sig)].append(s)
+
+    return list(d.values())
+
+    #brute force
     i = 0
     l1 = []
     used = []
---
+++ Hash Tables/bascis.md
@@ -0,0 +1,10 @@
+***Hash Tables***
+
+- data structure represents a dynamic set of data 
+- supports insert, delete, search 
+
+***Dict vs Hash Tables***
+
+- dict: generic way to map values
+- hash tables: implementation of a dict using hash function
+
