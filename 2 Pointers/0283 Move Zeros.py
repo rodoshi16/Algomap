760d6885e02d2eb0e027bfa3cd6631692a59d769
rodoshi16 <rodoshi524@gmail.com>
2026-02-01 17:53:24 -0500

    leetcode 0283

---
+++ 2 Pointers/0283 Move Zeros.py
@@ -0,0 +1,38 @@
+def moveZeroes(nums: list[int]):
+    """
+    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+    Note that you must do this in-place without making a copy of the array.
+
+
+
+    Example 1:
+
+    Input: nums = [0,1,0,3,12]
+    Output: [1,3,12,0,0]
+    Example 2:
+
+    Input: nums = [0]
+    Output: [0]
+
+
+    Constraints:
+
+    1 <= nums.length <= 104
+    -231 <= nums[i] <= 231 - 1
+
+
+    Follow up: Could you minimize the total number of operations done?
+    """
+    l, r = 0,0
+    while r < len(nums):
+        if nums[r] !=0:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r += 1
+        else:
+            r += 1
+    return nums
+
+
+
