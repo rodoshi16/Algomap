5a8e93d5b926149c5b42cb7ce34f1a189a56615a
rodoshi16 <rodoshi524@gmail.com>
2026-02-01 22:04:31 -0500

    leetcode 0098

---
+++ Binary Trees/0098 Validate Binary Search Tree.py
@@ -0,0 +1,56 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def isValidBST(root: [TreeNode]) -> bool:
+    """
+
+    Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+    A valid BST is defined as follows:
+
+    The left subtree of a node contains only nodes with keys strictly less than the node's key.
+    The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+    Both the left and right subtrees must also be binary search trees.
+
+
+    Example 1:
+
+
+    Input: root = [2,1,3]
+    Output: true
+    Example 2:
+
+
+    Input: root = [5,1,4,null,null,3,6]
+    Output: false
+    Explanation: The root node's value is 5 but its right child's value is 4.
+
+
+    Constraints:
+
+    The number of nodes in the tree is in the range [1, 104].
+    -231 <= Node.val <= 231 - 1
+
+    Time: 0(n) for each node in the tree is visited
+    Space: 0(h): number of recurisve call stacks is the height of the tree
+
+
+
+
+    """
+
+    def helper(node, low, high):
+        if node is None:
+            return True
+
+        if not (low <node.val<high):
+            return False
+
+        return helper(node.left, low, node.val) and helper(node.right, node.val, high)
+
+    return helper(root, float("-inf"), float("inf"))
+
