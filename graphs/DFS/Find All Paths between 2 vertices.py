bce487561f9812b896129c2ddff1005010fd77ab
rodoshi16 <rodoshi524@gmail.com>
2026-02-16 18:21:03 -0500

    Find all paths between 2 vertices

---
+++ graphs/DFS/Find All Paths between 2 vertices.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+
+def find_paths(n: int, edges: list[list[int]], source, destination) -> list[list]:
+    """
+    >>> find_paths(6, [["A","B"],["A","C"], ["A","D"], ["D","E"], ["C","E"], ["E","F"], ["B","F"]], "A", "B")
+    [['A', 'B'], ['A', 'C', 'E', 'F', 'B'], ['A', 'D', 'E', 'F', 'B']]
+
+
+    """
+
+    d = {}
+
+    for u, v in edges:
+        d.setdefault(u, []).append(v)
+        d.setdefault(v, []).append(u)
+
+    path = []
+    paths = []
+
+    def dfs(node):
+        path.append(node)
+
+        if node == destination:
+            paths.append(path.copy())
+
+        else:
+            for neighbour in d[node]:
+                if neighbour not in path:
+                    dfs(neighbour)
+
+        path.pop()
+
+    dfs(source)
+    return paths
+
+
+
+
+
+
