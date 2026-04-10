0c08625fa5de130910cf1b8945951a829abc0822
rodoshi16 <rodoshi524@gmail.com>
2026-04-10 00:34:08 -0400

    leetcode 1189

---
+++ Arrays and Strings/1189 Maximum Number of Balloons.py
@@ -0,0 +1,52 @@
+from collections import Counter
+def maxNumberOfBalloons(text: str) -> int:
+    #input: str
+    # f: form as many instances of baloon from text
+    # return max number of inputs
+
+    #count frequency of characters
+    # nlaebolko: {n: 1, l:2, a: 1, b:1, k:1}
+    # b:1 a:1 l:2 o:2 n:1
+    # b:2 a:2 l:4 o:4 n:2, n and 2n
+
+    #only count the characters of balloon
+    # capped by the minimum number
+    # if any characters of balloon not present, return 0
+
+
+    d = {}
+    for ch in text:
+        if ch not in d:
+            d[ch] = 1
+        else:
+            d[ch] += 1
+
+    for ch in "balloon":
+        if ch not in d :
+            return 0
+
+    return min(d["b"], d["a"], d["l"]//2, d["o"]//2, d["n"])
+
+
+    #more efficient (smarter)
+    d = Counter(text)
+    res = math.inf
+
+    for ch in "balloon":
+        if ch in "lo":
+            n = 2
+        else:
+            n = 1
+
+        res = min(res, d[ch]//n)
+
+    return res
+
+
+
+
+
+
+
+
+
